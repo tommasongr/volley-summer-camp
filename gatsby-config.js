d30318f9c037c936a1552903ba579e9c0ffcd5d6
Tommaso Negri <tommaso.negri98@gmail.com>
2018-06-27 11:41:15 +0200

    Test CMS 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
       }
     },
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/blog/`
+      }
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
